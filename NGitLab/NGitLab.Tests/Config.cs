56eb0a795d28dc2665e011b964921be32c2ca17b
MysticBoy <mysticboy@live.com>
2018-11-11 23:21:02 +0800

    delinfo

---
+++ NGitLab/NGitLab.Tests/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace NGitLab.Tests {
+    public static class Config {
+        public const string ServiceUrl = "https://gitclub.cn/";
+        public const string Secret = "";
+
+        public static GitLabClient Connect() {
+            return GitLabClient.Connect(ServiceUrl,"","", Impl.Api.ApiVersion.V3_1);
+        }
+    }
+}
