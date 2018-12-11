97717008f4a56658f70577fed9e3b453ba455962
MysticBoy <mysticboy@live.com>
2018-12-11 21:53:43 +0800

    update json

--- NGitLab/NGitLab.Tests/Config.cs
+++ NGitLab/NGitLab.Tests/Config.cs
@@ -1,10 +1,10 @@
 ﻿namespace NGitLab.Tests {
     public static class Config {
-        public const string ServiceUrl = "https://gitclub.cn/";
-        public const string Secret = "";
+        public const string ServiceUrl = "https://gitee.com/";
+        public const string Secret = "y1ZcmHSidM4bqwYzjFPU";
 
         public static GitLabClient Connect() {
-            return GitLabClient.Connect(ServiceUrl,"","", Impl.Api.ApiVersion.V3_1);
+            return GitLabClient.Connect(ServiceUrl,"maikebing","kissme", Impl.Api.ApiVersion.V3_1);
         }
     }
 }
