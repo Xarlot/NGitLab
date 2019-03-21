3e311d46571addab00d4ed711ae79cd8a322e34e
Xarlot <maxim.litvinov@devexpress.com>
2019-03-21 11:15:16 +0300

    fix tests

--- NGitLab/NGitLab.Tests/Config.cs
+++ NGitLab/NGitLab.Tests/Config.cs
@@ -1,10 +1,12 @@
-﻿namespace NGitLab.Tests {
-    public static class Config {
-        public const string ServiceUrl = "https://gitee.com/";
-        public const string Secret = "y1ZcmHSidM4bqwYzjFPU";
+﻿using NGitLab.Impl;
 
-        public static GitLabClient Connect() {
-            return GitLabClient.Connect(ServiceUrl,"maikebing","kissme", Impl.ApiVersion.V3_1);
+namespace NGitLab.Tests {
+    public static class Config {
+        public const string ServiceUrl = "http://gitserver";
+        public const string Secret = "kpdcucE1Y4wykmqBGD4x";
+
+        public static GitLabClient Connect() { 
+            return GitLabClient.Connect(ServiceUrl, Secret, ApiVersion.V4);
         }
     }
 }
