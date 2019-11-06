82e84a4836782adc2a14da3c48da5eda44f47bb6
Fernando Gevard <fernando.gevard@benner.com.br>
2019-11-06 17:48:05 -0300

    Merge Request API - Add "has_conflicts", "merge_status" and "pipeline" informations

---
+++ NGitLab/NGitLab/Models/MergeRequestPipeline.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace NGitLab.Models
+{
+    [DataContract]
+    public class MergeRequestPipeline
+    {
+        [DataMember(Name = "id")]
+        public int id { get; set; }
+        [DataMember(Name = "sha")]
+        public string sha { get; set; }
+        [DataMember(Name = "ref")]
+        public string @ref { get; set; }
+        [DataMember(Name = "status")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public MergeRequestPipelineStatus status { get; set; }
+        [DataMember(Name = "web_url")]
+        public string web_url { get; set; }
+    }
+}
