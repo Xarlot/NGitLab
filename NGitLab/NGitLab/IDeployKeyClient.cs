1acdfbb452068101c3c8571ad3dbb6b7aff37d9c
Lin Meyer <lin.meyer@redbox.com>
2019-12-14 20:16:05 -0600

    Added Update and Delete for Deploy Keys

--- NGitLab/NGitLab/IDeployKeyClient.cs
+++ NGitLab/NGitLab/IDeployKeyClient.cs
@@ -17,5 +17,15 @@ namespace NGitLab {
         ///     Enables a deploy key on a project.  Returns the enabled deploy key on success.
         /// </summary>
         DeployKey Enable(int projectId, int deployKeyId);
+
+        /// <summary>
+        ///     Updates a deploy key on a project.  Returns the updated deploy key on success.
+        /// </summary>
+        DeployKey Update(int projectId, DeployKey key);
+
+        /// <summary>
+        ///     Deletes a deploy key from a project.
+        /// </summary>
+        void Delete(int projectId, int deployKeyId);
     }
 }
--- NGitLab/NGitLab/Impl/DeployKeyClient.cs
+++ NGitLab/NGitLab/Impl/DeployKeyClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NGitLab.Models;
 
 namespace NGitLab.Impl {
@@ -6,6 +7,7 @@ namespace NGitLab.Impl {
         const string DeployKeysUrl = "/deploy_keys";
         const string ProjectDeployKeysUrl = "/projects/{0}/deploy_keys";
         const string ProjectDeployKeyEnableUrl = "/projects/{0}/deploy_keys/{1}/enable";
+        const string ProjectDeployKeyUpdateUrl = "/projects/{0}/deploy_keys/{1}";
         readonly Api api;
 
         public DeployKeyClient(Api api) {
@@ -23,6 +25,21 @@ namespace NGitLab.Impl {
         public DeployKey Enable(int projectId, int deployKeyId)
         {
             return api.Post().To<DeployKey>(string.Format(ProjectDeployKeyEnableUrl, projectId, deployKeyId));
+        }
+
+        public DeployKey Update(int projectId, DeployKey key)
+        {
+            if(key == null)
+            {
+                throw new ArgumentException($"{nameof(key)} is required and cannot be null");
+            }
+
+            return api.Put().With(key).To<DeployKey>(string.Format(ProjectDeployKeyUpdateUrl, projectId, key.Id));
+        }
+
+        public void Delete(int projectId, int deployKeyId)
+        {
+            api.Delete().To<DeployKey>(string.Format(ProjectDeployKeyUpdateUrl, projectId, deployKeyId));
         }
     }
 }
