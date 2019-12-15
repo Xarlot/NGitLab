aa27940b3d6512dada78dccff93b17dfd80a8014
Lin Meyer <linjmeyer@gmail.com>
2019-12-15 00:24:26 -0600

    Added deploy keys client

---
+++ NGitLab/NGitLab/IDeployKeyClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using NGitLab.Models;
+
+namespace NGitLab {
+    public interface IDeployKeyClient {
+        /// <summary>
+        ///     Get a list of all deploy keys
+        /// </summary>
+        IEnumerable<DeployKey> Get();
+
+        /// <summary>
+        ///     Get a list of deploy keys for the specified project.
+        /// </summary>
+        IEnumerable<DeployKey> Get(int projectId);
+
+        /// <summary>
+        ///     Enables a deploy key on a project.  Returns the enabled deploy key on success.
+        /// </summary>
+        DeployKey Enable(int projectId, int deployKeyId);
+    }
+}
---
+++ NGitLab/NGitLab/Impl/DeployKeyClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NGitLab.Models;
+
+namespace NGitLab.Impl {
+    public class DeployKeyClient : IDeployKeyClient {
+        const string DeployKeysUrl = "/deploy_keys";
+        const string ProjectDeployKeysUrl = "/projects/{0}/deploy_keys";
+        const string ProjectDeployKeyEnableUrl = "/projects/{0}/deploy_keys/{1}/enable";
+        readonly Api api;
+
+        public DeployKeyClient(Api api) {
+            this.api = api;
+        }
+
+        public IEnumerable<DeployKey> Get() {
+            return api.Get().GetAll<DeployKey>(DeployKeysUrl);
+        }
+
+        public IEnumerable<DeployKey> Get(int projectId) {
+            return api.Get().GetAll<DeployKey>(string.Format(ProjectDeployKeysUrl, projectId));
+        }
+
+        public DeployKey Enable(int projectId, int deployKeyId)
+        {
+            return api.Post().To<DeployKey>(string.Format(ProjectDeployKeyEnableUrl, projectId, deployKeyId));
+        }
+    }
+}
