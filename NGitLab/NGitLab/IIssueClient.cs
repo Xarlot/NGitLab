415ec3af667f37367cb6dfba6e597ef2bd748791
Fernando Gevard <fernando.gevard@benner.com.br>
2021-03-11 12:42:37 -0300

    Milestone API + Issue API

--- NGitLab/NGitLab/IIssueClient.cs
+++ NGitLab/NGitLab/IIssueClient.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections.Generic;
 using NGitLab.Models;
 
-namespace NGitLab {
-    public interface IIssueClient {
+namespace NGitLab
+{
+    public interface IIssueClient
+    {
+
+        IEnumerable<Issue> All();
+
+        IEnumerable<Issue> AllInState(IssueState state);
+
+        Issue Get(int id);
+
         /// <summary>
         ///     Get a list of all project issues
         /// </summary>
@@ -12,5 +21,24 @@ namespace NGitLab {
         ///     Get a list of issues for the specified project.
         /// </summary>
         IEnumerable<Issue> ForProject(int projectId);
+
+        Issue Create(IssueCreate issue);
+
+        Issue Delete(int issueIid);
+
+
+        Issue Update(int issueId, IssueUpdate issue);
+
+        IssueTimeTrack TimeEstimateSet(int issueId, string duration);
+
+        IssueTimeTrack TimeEstimateReset(int issueId);
+
+        IssueTimeTrack TimeSpentAdd(int issueId, string duration);
+
+        IssueTimeTrack TimeSpentReset(int issueId);
+        IssueTimeTrack TimeStats(int issueId);
+        Issue ReopenIssue(int issueIid);
+        Issue CloseIssue(int issueIid);
+
     }
 }
---
+++ NGitLab/NGitLab/Models/IssueCreate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace NGitLab.Models {
+    [DataContract]
+    public class IssueCreate {
+        [DataMember(Name = "created_at")]
+        public DateTime? CreatedAt { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+        
+        [DataMember(Name = "due_date")]
+        public DateTime? dueDate { get; set; }
+
+        //Comma-separated list of label names
+        [DataMember(Name = "labels")]
+        public string[] Labels { get; set; }
+
+        [DataMember(Name = "milestone_id")]
+        public int milestoneId { get; set; }
+
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+
+        [DataMember(Name = "assignee_ids")]
+        public int[] assigneeIds { get; set; }
+    }
+}
